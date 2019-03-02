a666d81391376d808aac094359ae4d21cdf61e47
Manuel Carvalho <39609469+ButSekks@users.noreply.github.com>
2019-03-02 19:32:10 +0000

    Update 50pi.c

--- Resolucoes_Fichas/50pi.c
+++ Resolucoes_Fichas/50pi.c
@@ -141,7 +141,7 @@ int qDig (unsigned int n){
 //------------------------------------------------
 
 char* myStrcat (char* s1 , char* s2){
-    int i,j;
+    int i=0,j;
 
     while(s1[i]!='\0')
         i++;
